ff78e29fd7a773ebc6e86e65208a4d689c689428
Alessia Lacitignola <alelaci21@gmail.com>
2023-02-24 17:37:30 +0100

    BE-Week7-Progetto

---
+++ w7progetto/src/test/java/it/alessiaepicode/w7progetto/W7progettoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.alessiaepicode.w7progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W7progettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
